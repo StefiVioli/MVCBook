4c5289f2015e2c2e45d60fa59da15591164a355a
StefiVioli <stvioli@alumno.unlam.edu.ar>
2021-11-04 20:16:59 -0300

    Agregar archivos de proyecto.

---
+++ MVCBook/Controllers/BookController.cs
@@ -0,0 +1,60 @@
+﻿using MVCBook.Data;
+using MVCBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBook.Controllers
+{
+    public class BookController : Controller
+    {      
+        //Crear instancia del dbcontext
+
+        private BookDbContext context = new BookDbContext();
+
+        // GET:Book  o /Book/Index
+        public ActionResult Index()
+        {
+            //Traer todos los Books usando EF
+            var books = context.Books.ToList();
+
+            //el controller retorna una vista "Index" con la lista de books
+            return View("Index", books);
+        }
+
+        //Creamos dos métodos para la inserción del book en la DB
+
+        //primer create por GET para retornar la vista de registro
+        [HttpGet]//El Get es implicito asi y todo lo podemos indicar
+        public ActionResult Create()
+        {
+            //creamos la instancia con valores en las propiedades
+            Book book = new Book();
+
+            //retornamos la vista "Create" que tiene el objeto book
+            return View("Create", book);
+        }
+
+        //Segundo Create es por Post para insertar el nuevo libro en la base
+        //cuando el usuario en la vista Create hace click en enviar
+        //Book/Create -->POST
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Books.Add(book);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("Create", book);
+        }
+
+
+
+
+    }
+}
---
+++ MVCBook/Models/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCBook.Models
+{
+    [Table("Book")]//EF-->DB
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required")]
+
+        [Column(TypeName = "varchar")]
+        public string Name { get; set; }
+
+        [MaxLength(50, ErrorMessage="The field Author must be a string with a maximum lenght of 50")]
+
+        [Column(TypeName = "varchar")]
+        public string Author { get; set; }
+
+        [Range(100, 10000, ErrorMessage ="The field PagesNumber must be between 100 and 10000")]
+        public int PagesNumber { get; set; }
+
+        [Column(TypeName = "varchar")]
+        public string Publisher { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [RegularExpression(@"^\d{4}(\-|\/|\.)\d{1,2}\1\d{1,2}$", ErrorMessage = "The field PublicationDate must match the regular expression, por ejemplo: 2017-06-16")]
+        public string PublicationDate { get; set; }
+
+        [Column(TypeName = "varchar")]
+        public string Content { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Price", ErrorMessage ="'PriceConfirm' and 'Price' do not match")]
+        public decimal PriceConfirm { get; set; }
+    }
+}
